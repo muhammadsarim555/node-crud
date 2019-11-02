10037b96fcdc568b372edc948ee96ea50cede9ea
muhammadsarim555 <muhammadsarim555@gmail.com>
2019-11-02 13:33:34 -0700

    Install - JWT for Encodeing & Decoding Token

--- playground/hash.js
+++ playground/hash.js
@@ -1,8 +1,16 @@
 const SHA256 = require("crypto-js/sha256");
+const jwt = require('jsonwebtoken');
 
-const message = "i";
-const convert = SHA256(message).toString()
+// const message = "i";
+// const convert = SHA256(message).toString()
 
 // console.log(`Message ${message}`)
-console.log(`Convert ${convert}`)
+// console.log(`Convert ${convert}`)
 
+
+ const obj = {name: "sarim"};
+ const token = jwt.sign(obj, "123321")
+ const decode = jwt.verify(token, "123321")
+ 
+ console.log(token)
+ console.log("decode", decode)
